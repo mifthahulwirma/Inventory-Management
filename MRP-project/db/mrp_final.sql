227202bd1badc586e266198db9a5ea5fa6152585
mifthahulwirma <mifthahulwirma21@gmail.com>
2024-06-08 00:40:33 +0700

    final project

---
+++ MRP-project/db/mrp_final.sql
@@ -0,0 +1,477 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 02, 2024 at 06:34 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mrp_final`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_cust` int(25) NOT NULL,
+  `cust_name` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `telp` int(20) NOT NULL,
+  `email` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id_cust`, `cust_name`, `address`, `telp`, `email`) VALUES
+(1, 'Test Customer', 'Indonesia', 123456789, 'test@mail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `finished_report`
+--
+
+CREATE TABLE `finished_report` (
+  `id_finished` int(11) NOT NULL,
+  `id_project` int(11) NOT NULL,
+  `total_finished` int(11) NOT NULL,
+  `fdate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `finished_report`
+--
+
+INSERT INTO `finished_report` (`id_finished`, `id_project`, `total_finished`, `fdate`) VALUES
+(1001, 1001, 60, '2024-05-02 16:32:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `machine`
+--
+
+CREATE TABLE `machine` (
+  `id_machine` int(50) NOT NULL,
+  `machine_name` varchar(50) NOT NULL,
+  `capacity` int(15) NOT NULL,
+  `mc_status` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `machine`
+--
+
+INSERT INTO `machine` (`id_machine`, `machine_name`, `capacity`, `mc_status`) VALUES
+(1001, 'Machine1', 100, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material`
+--
+
+CREATE TABLE `material` (
+  `id_material` int(50) NOT NULL,
+  `material_name` varchar(50) NOT NULL,
+  `stock` int(50) NOT NULL,
+  `price` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `material`
+--
+
+INSERT INTO `material` (`id_material`, `material_name`, `stock`, `price`) VALUES
+(1001, 'beras', 100, '5000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `planning`
+--
+
+CREATE TABLE `planning` (
+  `id_plan` int(15) NOT NULL,
+  `plan_name` varchar(25) NOT NULL,
+  `id_project` int(15) NOT NULL,
+  `qty_target` int(11) NOT NULL,
+  `end_date` date NOT NULL,
+  `pl_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `planning`
+--
+
+INSERT INTO `planning` (`id_plan`, `plan_name`, `id_project`, `qty_target`, `end_date`, `pl_status`) VALUES
+(1001, 'Plan-SATU', 1001, 70, '2024-05-31', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plan_shift`
+--
+
+CREATE TABLE `plan_shift` (
+  `id_planshift` int(15) NOT NULL,
+  `id_plan` int(15) NOT NULL,
+  `id_shift` int(15) NOT NULL,
+  `id_staff` int(15) NOT NULL,
+  `start_date` date NOT NULL,
+  `ps_status` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `plan_shift`
+--
+
+INSERT INTO `plan_shift` (`id_planshift`, `id_plan`, `id_shift`, `id_staff`, `start_date`, `ps_status`) VALUES
+(1001, 1001, 3, 1001, '2024-05-03', 1),
+(1002, 1001, 2, 1002, '2024-05-03', 0),
+(1003, 1001, 3, 1003, '2024-05-02', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id_product` int(25) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `summary` longtext NOT NULL,
+  `application` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id_product`, `product_name`, `summary`, `application`) VALUES
+(1001, 'tas', 'maidsabdsbhjefejjscsj', 'kmacjsacuafniuajsciua');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `id_project` int(25) NOT NULL,
+  `project_name` varchar(50) NOT NULL,
+  `id_cust` int(25) NOT NULL,
+  `id_product` int(25) NOT NULL,
+  `diameter` int(25) NOT NULL,
+  `qty_request` int(15) NOT NULL,
+  `entry_date` date NOT NULL,
+  `pr_status` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`id_project`, `project_name`, `id_cust`, `id_product`, `diameter`, `qty_request`, `entry_date`, `pr_status`) VALUES
+(1001, 'PJ-TATA', 1, 1001, 10, 120, '2024-05-02', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `p_machine`
+--
+
+CREATE TABLE `p_machine` (
+  `id_pmachine` int(15) NOT NULL,
+  `id_planshift` int(15) NOT NULL,
+  `id_machine` int(15) NOT NULL,
+  `mc_stats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `p_machine`
+--
+
+INSERT INTO `p_machine` (`id_pmachine`, `id_planshift`, `id_machine`, `mc_stats`) VALUES
+(1001, 1002, 1001, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `p_material`
+--
+
+CREATE TABLE `p_material` (
+  `id_pmaterial` int(15) NOT NULL,
+  `id_planshift` int(15) NOT NULL,
+  `id_material` int(15) NOT NULL,
+  `used_stock` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `p_material`
+--
+
+INSERT INTO `p_material` (`id_pmaterial`, `id_planshift`, `id_material`, `used_stock`) VALUES
+(1001, 1002, 1001, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shiftment`
+--
+
+CREATE TABLE `shiftment` (
+  `id_shift` int(11) NOT NULL,
+  `shift_name` varchar(50) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shiftment`
+--
+
+INSERT INTO `shiftment` (`id_shift`, `shift_name`, `start_time`, `end_time`) VALUES
+(1, 'Pagi', '09:00:00', '17:00:00'),
+(2, 'Siang', '14:00:00', '22:00:00'),
+(3, 'Malam', '22:00:00', '06:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sorting_report`
+--
+
+CREATE TABLE `sorting_report` (
+  `id_sorting` int(15) NOT NULL,
+  `id_planshift` int(15) NOT NULL,
+  `waste` int(50) NOT NULL,
+  `finished` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sorting_report`
+--
+
+INSERT INTO `sorting_report` (`id_sorting`, `id_planshift`, `waste`, `finished`) VALUES
+(1001, 1002, 10, 60);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id_staff` int(11) NOT NULL,
+  `staff_name` varchar(50) NOT NULL,
+  `phone` int(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `st_status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0=Ready\r\n1=Schedule '
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id_staff`, `staff_name`, `phone`, `email`, `st_status`) VALUES
+(1001, 'tata', 1234567, 'tata@BACOT.COM', 1),
+(1002, 'battler', 1234567, 'okay@oke.com', 1),
+(1003, 'battler 2', 1234567, 'okay@oke.com', 1),
+(1004, 'battler 2', 1234567, 'okay@oke.com', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `password` varchar(11) NOT NULL,
+  `role` enum('admin','leader') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `role`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(2, 'leader', 'leader', 'leader');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_cust`);
+
+--
+-- Indexes for table `finished_report`
+--
+ALTER TABLE `finished_report`
+  ADD PRIMARY KEY (`id_finished`);
+
+--
+-- Indexes for table `machine`
+--
+ALTER TABLE `machine`
+  ADD PRIMARY KEY (`id_machine`);
+
+--
+-- Indexes for table `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`id_material`);
+
+--
+-- Indexes for table `planning`
+--
+ALTER TABLE `planning`
+  ADD PRIMARY KEY (`id_plan`);
+
+--
+-- Indexes for table `plan_shift`
+--
+ALTER TABLE `plan_shift`
+  ADD PRIMARY KEY (`id_planshift`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id_project`);
+
+--
+-- Indexes for table `p_machine`
+--
+ALTER TABLE `p_machine`
+  ADD PRIMARY KEY (`id_pmachine`);
+
+--
+-- Indexes for table `p_material`
+--
+ALTER TABLE `p_material`
+  ADD PRIMARY KEY (`id_pmaterial`);
+
+--
+-- Indexes for table `shiftment`
+--
+ALTER TABLE `shiftment`
+  ADD PRIMARY KEY (`id_shift`);
+
+--
+-- Indexes for table `sorting_report`
+--
+ALTER TABLE `sorting_report`
+  ADD PRIMARY KEY (`id_sorting`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id_staff`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_cust` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `machine`
+--
+ALTER TABLE `machine`
+  MODIFY `id_machine` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1011;
+
+--
+-- AUTO_INCREMENT for table `material`
+--
+ALTER TABLE `material`
+  MODIFY `id_material` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1008;
+
+--
+-- AUTO_INCREMENT for table `planning`
+--
+ALTER TABLE `planning`
+  MODIFY `id_plan` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003;
+
+--
+-- AUTO_INCREMENT for table `plan_shift`
+--
+ALTER TABLE `plan_shift`
+  MODIFY `id_planshift` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id_product` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1002;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `id_project` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003;
+
+--
+-- AUTO_INCREMENT for table `shiftment`
+--
+ALTER TABLE `shiftment`
+  MODIFY `id_shift` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id_staff` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1015;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
